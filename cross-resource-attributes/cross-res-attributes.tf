0a06ab37c65c440fb44701218b033bf9a3d1b8d4
Trisala Thapa <165699465+TrisalaThapa7@users.noreply.github.com>
2024-05-16 16:06:55 -0400

    cross resource attributes

---
+++ cross-resource-attributes/cross-res-attributes.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_eip" "lb" {
+    domain = "vpc"
+}
+
+resource "aws_security_group" "allow_tls" {
+    name = "attribute-sg"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls" {
+    security_group_id = aws_security_group.allow_tls.id
+    cidr_ipv4 = "${aws_eip.lb.public_ip}/32"
+    from_port = 443
+    ip_protocol = "tcp"
+    to_port = 443
+}
